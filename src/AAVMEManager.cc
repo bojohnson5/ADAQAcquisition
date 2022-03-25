37cd26e3bcb325f7186757f7d147672cf1c50cbe
bojohnson5 <bo.johnson5@icloud.com>
2022-03-25 15:31:40 -0400

    more print statements

--- src/AAVMEManager.cc
+++ src/AAVMEManager.cc
@@ -4,7 +4,7 @@
 //                 Zachary Seth Hartwig : All rights reserved                  //
 //                                                                             //
 //      The ADAQAcquisition source code is licensed under the GNU GPL v3.0.    //
-//      You have the right to modify and/or redistribute this source code      //      
+//      You have the right to modify and/or redistribute this source code      //
 //      under the terms specified in the license, which may be found online    //
 //      at http://www.gnu.org/licenses or at $ADAQACQUISITION/License.txt.     //
 //                                                                             //
@@ -48,15 +48,15 @@ Int_t AAVMEManager::InitializeBridge()
 {
   BRMgr = new ADAQBridge((ZBoardType)BRType,
 			 BRIdentifier);
-  
+
   BRMgr->SetVerbose(true);
-  
+
   Int_t Status = -42;
-  
+
   // Connect to the VME/USB bridge WITHOUT a digitizer enabled ...
   if(!TheVMEManager->GetDGEnable())
     Status = BRMgr->OpenLinkDirectly();
-  
+
   // Connect to the VME/USB bridge VIA the digitizer
   else if(DGEnable == 0)
     Status = BRMgr->OpenLinkViaDigitizer(DGMgr->GetBoardHandle(),
@@ -64,7 +64,7 @@ Int_t AAVMEManager::InitializeBridge()
 
   if(BRMgr->GetLinkEstablished())
     BRLinkOpen = true;
-  
+
   return Status;
 }
 
@@ -72,18 +72,18 @@ Int_t AAVMEManager::InitializeBridge()
 Int_t AAVMEManager::InitializeDigitizer()
 {
   DGMgr = new ADAQDigitizer((ZBoardType)DGType,
-			    DGIdentifier, 
+			    DGIdentifier,
 			    DGAddress,
-			    DGLinkNumber, 
+			    DGLinkNumber,
 			    DGCONETNode);
-  
+
   DGMgr->SetVerbose(true);
-  
+
   Int_t Status = DGMgr->OpenLink();
 
   if(DGMgr->GetLinkEstablished())
     DGLinkOpen = true;
-  
+
   return Status;
 }
 
@@ -94,12 +94,12 @@ Int_t AAVMEManager::InitializeHighVoltage()
 			      HVIdentifier,
 			      HVAddress,
 			      HVLinkNumber);
-  
-  
+
+
   HVMgr->SetVerbose(true);
 
   Int_t Status = -42;
-  
+
   if(HVType == zDT5790M or HVType == zDT5790N or HVType == zDT5790P){
 
     if(!DGMgr->GetLinkEstablished())
@@ -109,23 +109,24 @@ Int_t AAVMEManager::InitializeHighVoltage()
       HVMgr->SetBoardHandle(DGHandle);
     }
   }
-  
+
   Status = HVMgr->OpenLink();
 
   if(HVMgr->GetLinkEstablished())
     HVLinkOpen = true;
-  
+
   return Status;
 }
 
 
 bool AAVMEManager::ProgramDigitizers()
 {
+  cout << "Programming digitizers\n";
   DGMgr->Reset();
-  
+
   uint32_t DGNumChEnabled = 0;
   uint32_t DGChEnableMask = 0;
-  
+
   ////////////////////////////
   // Channel-specific settings
 
@@ -143,36 +144,36 @@ bool AAVMEManager::ProgramDigitizers()
     if(TheSettings->STDFirmware){
       DGMgr->SetChannelDCOffset(ch, TheSettings->ChDCOffset[ch]);
       DGMgr->SetChannelTriggerThreshold(ch, TheSettings->ChTriggerThreshold[ch]);
-      
+
       if(TheSettings->ChPosPolarity[ch])
         DGMgr->SetChannelPulsePolarity(ch, CAEN_DGTZ_PulsePolarityPositive);
       else
         DGMgr->SetChannelPulsePolarity(ch, CAEN_DGTZ_PulsePolarityNegative);
-    
+
       if(TheSettings->ZeroSuppressionEnable){
         DGMgr->SetZSMode("ZLE");
-	
+
         DGMgr->SetZLEChannelSettings(ch,
 				     TheSettings->ChZLEThreshold[ch],
 				     TheSettings->ChZLEBackward[ch],
 				     TheSettings->ChZLEForward[ch],
 				     TheSettings->ChZLEPosLogic[ch]);
-	
+
 	// Testing for positive ZLE logic
 	if(TheSettings->ChZLEPosLogic[ch]){
 	  if(TheSettings->ChZLEThreshold[ch] > TheSettings->ChTriggerThreshold[ch]){
-	    
+
 	    cout << "Error! For ZLE positive logic: ZLE Threshold < Ch Trigger!\n"
 		 << endl;
-	    
+
 	    return false;
 	  }
 	}
-	
+
 	// Testing for negative ZLE logic
 	else if(TheSettings->ChZLENegLogic[ch]){
 	  if(TheSettings->ChZLEThreshold[ch] < TheSettings->ChTriggerThreshold[ch]){
-	    
+
 	    cout << "Error! For ZLE negative logic: ZLE Threshold > Ch Trigger!\n"
 		 << endl;
 	    return false;
@@ -184,7 +185,7 @@ bool AAVMEManager::ProgramDigitizers()
 
   // Set the channel-enable mask
   DGMgr->SetChannelEnableMask(DGChEnableMask);
-  
+
   // Ensure that at least one channel is enabled in the channel
   // enabled bit mask; if not, alert the user and return without
   // beginning acquisition since there ain't nothin' to acquire.
@@ -207,13 +208,13 @@ bool AAVMEManager::ProgramDigitizers()
     DGMgr->DisableAutoTrigger(DGChEnableMask);
     DGMgr->DisableSWTrigger();
     break;
-    
+
   case 2: // Automatic
     DGMgr->DisableExternalTrigger();
     DGMgr->EnableAutoTrigger(DGChEnableMask);
     DGMgr->DisableSWTrigger();
     break;
-    
+
   case 3: // Software
     DGMgr->DisableExternalTrigger();
     DGMgr->DisableAutoTrigger(DGChEnableMask);
@@ -227,25 +228,25 @@ bool AAVMEManager::ProgramDigitizers()
   // Trigger edge (channel-specific but treated as group setting)
 
   if(TheSettings->STDFirmware){
-  
+
     for(int ch=0; ch<DGMgr->GetNumChannels(); ch++){
-      
+
       switch(TheSettings->TriggerEdge){
-	
+
       case 0: // Rising edge
 	DGMgr->SetTriggerEdge(ch, "Rising");
 	break;
-	
+
       case 1: // Falling edge
 	DGMgr->SetTriggerEdge(ch, "Falling");
 	break;
-	
+
       default:
 	break;
       }
     }
   }
-  
+
   if(TheSettings->TriggerCoincidenceEnable and
      TheSettings->TriggerCoincidenceLevel < DGNumChEnabled)
     DGMgr->SetTriggerCoincidence(true, TheSettings->TriggerCoincidenceLevel);
@@ -255,19 +256,20 @@ bool AAVMEManager::ProgramDigitizers()
   // Acquisition settings
 
   switch(TheSettings->AcquisitionControl){
-    
+
   case 0: // Standard (software controlled)
+    cout << "Setting Software control\n";
     DGMgr->SetAcquisitionControl("Software");
     break;
-    
+
   case 1: // Gated (NIM signal on S-IN Lemo 00 front panel)
     DGMgr->SetAcquisitionControl("Gated (NIM)");
     break;
-    
+
   case 2: // Gated (TTL signal on S-IN Lemo 00 front panel)
     DGMgr->SetAcquisitionControl("Gated (TTL)");
     break;
-    
+
   default:
     break;
   }
@@ -275,19 +277,19 @@ bool AAVMEManager::ProgramDigitizers()
   if(TheSettings->STDFirmware){
     DGMgr->SetRecordLength(TheSettings->RecordLength);
     DGMgr->SetPostTriggerSize(TheSettings->PostTrigger);
-    
+
     if(TheSettings->ZeroSuppressionEnable)
       DGMgr->SetZSMode("ZLE");
     else
       DGMgr->SetZSMode("None");
   }
-  
+
   ///////////////////
   // Readout settings
-  
+
   if(TheSettings->STDFirmware)
     DGMgr->SetMaxNumEventsBLT(TheSettings->EventsBeforeReadout);
-  
+
   if(TheSettings->PSDFirmware){
 
     //////////////////////////////////////////////
@@ -299,28 +301,28 @@ bool AAVMEManager::ProgramDigitizers()
     // Ch1. At present, DPP-PSD is under heavy development and
     // documentation is spotty at best. Future updates will try to
     // make this as clear to the user in the interface.
-    
+
     CAEN_DGTZ_DPP_PSD_Params_t PSDParameters;
-    
+
     for(Int_t ch=0; ch<DGMgr->GetNumChannels(); ch++){
-      
+
       PSDParameters.nsbl[ch] = TheSettings->ChBaselineSamples[ch];
       PSDParameters.csens[ch] = TheSettings->ChChargeSensitivity[ch];
-      
+
       // Channel self-triggering (automatic)
       if(TheSettings->TriggerType == 2)
 	PSDParameters.selft[ch] = 1;
-      
+
       // Software or external triggering
       else
 	PSDParameters.selft[ch] = 0;
-      
+
       PSDParameters.thr[ch] = TheSettings->ChTriggerThreshold[ch];
       PSDParameters.tvaw[ch] = TheSettings->ChTriggerValidation[ch];
       PSDParameters.trgc[ch] = (CAEN_DGTZ_DPP_TriggerConfig_t)TheSettings->ChTriggerConfig[ch];
-      
+
       PSDParameters.sgate[ch] = TheSettings->ChShortGate[ch];
-      PSDParameters.lgate[ch] = TheSettings->ChLongGate[ch]; 
+      PSDParameters.lgate[ch] = TheSettings->ChLongGate[ch];
       PSDParameters.pgate[ch] = TheSettings->ChGateOffset[ch];
     }
 
@@ -334,7 +336,7 @@ bool AAVMEManager::ProgramDigitizers()
     // present but may be used in the future. ZSH (28 Sep 15)
     //
     // PSDParameters.purh = CAEN_DGTZ_DPP_PSD_PUR_DetectOnly;
-    // PSDParameters.purgap = 100; 
+    // PSDParameters.purgap = 100;
 
     // It is unclear from CAEN documentation whether or not these
     // parameters are still in use. I have asked for
@@ -348,43 +350,43 @@ bool AAVMEManager::ProgramDigitizers()
 
     ///////////////////////////////////////////////////////
     // Set channel-specific, non-PSD structure PSD settings
-    
+
     for(Int_t ch=0; ch<DGMgr->GetNumChannels(); ch++){
-      
+
       DGMgr->SetRecordLength(TheSettings->ChRecordLength[ch], ch);
-      
+
       DGMgr->SetChannelDCOffset(ch, TheSettings->ChDCOffset[ch]);
-      
+
       DGMgr->SetDPPPreTriggerSize(ch, TheSettings->ChPreTrigger[ch]);
-      
+
       if(TheSettings->ChPosPolarity[ch])
 	DGMgr->SetChannelPulsePolarity(ch, CAEN_DGTZ_PulsePolarityPositive);
       else if(TheSettings->ChNegPolarity[ch])
 	DGMgr->SetChannelPulsePolarity(ch, CAEN_DGTZ_PulsePolarityNegative);
     }
 
-    
+
     ////////////////////////////////////////////
     // Set global non-PSD structure PSD settings
 
     DGMgr->SetDPPAcquisitionMode((CAEN_DGTZ_DPP_AcqMode_t)TheSettings->PSDOperationMode,
 				 CAEN_DGTZ_DPP_SAVE_PARAM_EnergyAndTime);
-    
+
     DGMgr->SetDPPTriggerMode(CAEN_DGTZ_DPP_TriggerMode_Normal);
-    
+
     DGMgr->SetIOLevel(CAEN_DGTZ_IOLevel_TTL);
-    
+
     DGMgr->SetDPPEventAggregation(TheSettings->EventsBeforeReadout, 0);
-    
+
     DGMgr->SetRunSynchronizationMode(CAEN_DGTZ_RUN_SYNC_Disabled);
 
-    
+
     ////////////////////////////////////
     // Set PSD analog and virtual probes
-    
+
     DGMgr->SetDPPVirtualProbe(ANALOG_TRACE_1,
 			      CAEN_DGTZ_DPP_VIRTUALPROBE_Input);
-    
+
     DGMgr->SetDPPVirtualProbe(ANALOG_TRACE_2,
 			      CAEN_DGTZ_DPP_VIRTUALPROBE_Baseline);
 
@@ -407,14 +409,14 @@ bool AAVMEManager::ProgramDigitizers()
     //     CAEN_DGTZ_DPP_DIGITALPROBE_Coincidence;
 
     Int_t Status = -42;
-    
+
     Status = DGMgr->SetDPPVirtualProbe(DIGITAL_TRACE_3,
 				       CAEN_DGTZ_DPP_DIGITALPROBE_PileUp);
-    
+
     Status = DGMgr->SetDPPVirtualProbe(DIGITAL_TRACE_4,
 				       CAEN_DGTZ_DPP_DIGITALPROBE_GateShort);
   }
-  
+
   return true;
 }
 
@@ -422,9 +424,9 @@ bool AAVMEManager::ProgramDigitizers()
 void AAVMEManager::SafelyDisconnectVMEBoards()
 {
   if(HVLinkOpen){
-    
+
     HVMgr->SetToSafeState();
-    
+
     if(HVType == zDT5790M or HVType == zDT5790N or HVType == zDT5790P){
     }
     else
@@ -432,12 +434,12 @@ void AAVMEManager::SafelyDisconnectVMEBoards()
 
     HVLinkOpen = false;
   }
-  
+
   if(DGLinkOpen){
     DGMgr->CloseLink();
     DGLinkOpen = false;
   }
-  
+
   if(BRLinkOpen){
     BRMgr->CloseLink();
     BRLinkOpen = false;
@@ -451,7 +453,7 @@ void AAVMEManager::StartHVMonitoring(AAInterface *TI)
 
   //  vector<uint16_t> Voltage(HVMgr->GetNumChannels(), 0);
   //  vector<uint16_t> Current(HVMgr->GetNumChannels(), 0);
-  
+
   // The high voltage and current will be displayed and updated in the
   // dedicated number entry fields when HVMonitorEnable is true
   while(HVMonitorEnable){
@@ -459,18 +461,18 @@ void AAVMEManager::StartHVMonitoring(AAInterface *TI)
     // Perform action in a separate thread to enable use of other GUI
     // features while HV monitoring is taking place
     gSystem->ProcessEvents();
-    
+
     // Update the voltage and current displays every half-second
     double delay = clock()+(0.5*CLOCKS_PER_SEC);
     while(clock()<delay){gSystem->ProcessEvents();}
-    
+
     uint16_t Voltage, Current;
     const int HVChannels = TheVMEManager->GetHVManager()->GetNumChannels();
     for(int ch=0; ch<HVChannels; ch++){
-      
+
       HVMgr->GetVoltage(ch, &Voltage);
       HVMgr->GetCurrent(ch, &Current);
-      
+
       TI->UpdateHVMonitors(ch, Voltage, Current);
     }
   }
